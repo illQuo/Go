b7dc2f36fa0bc25535daf7dcfe8bbd2a1377ee8f
Reece Burke <illquo26@gmail.com>
2021-09-16 22:43:48 +1000

    assignment

---
+++ udemy/even_or_odd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	sliceInt := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, x := range sliceInt {
+		if x%2 == 0 {
+			fmt.Printf("%d is even\n", x)
+		} else {
+			fmt.Printf("%d is odd\n", x)
+		}
+	}
+}
